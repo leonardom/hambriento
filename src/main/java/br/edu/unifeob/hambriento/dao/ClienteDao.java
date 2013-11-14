4ec6352d38c99940e52c67a0a76e425528429ae6
Osvaldo Gusmão <osvaldogusmao@Joses-MacBook-Pro.local>
2013-11-14 01:30:21 -0200

    Refactor - DAO e DAOGenerico

--- src/main/java/br/edu/unifeob/hambriento/dao/ClienteDao.java
+++ src/main/java/br/edu/unifeob/hambriento/dao/ClienteDao.java
@@ -1,12 +1,10 @@
-
 package br.edu.unifeob.hambriento.dao;
 
+import br.edu.unifeob.hambriento.dao.base.DAO;
 import br.edu.unifeob.hambriento.entity.Cliente;
 
 /*
-@author Ivan Simionato
-*/
-
-public class ClienteDao extends GenericDao<Cliente> {
-    
+ @author Ivan Simionato
+ */
+public class ClienteDao extends DAO<Cliente> {
 }
--- src/main/java/br/edu/unifeob/hambriento/dao/EstabelecimentoDao.java
+++ src/main/java/br/edu/unifeob/hambriento/dao/EstabelecimentoDao.java
@@ -1,11 +1,10 @@
-
 package br.edu.unifeob.hambriento.dao;
 
+import br.edu.unifeob.hambriento.dao.base.DAO;
 import br.edu.unifeob.hambriento.entity.Estabelecimento;
 
 /*    
-@author Ivan Simionato
-*/
-public class EstabelecimentoDao extends GenericDao<Estabelecimento>{
-    
+ @author Ivan Simionato
+ */
+public class EstabelecimentoDao extends DAO<Estabelecimento> {
 }
