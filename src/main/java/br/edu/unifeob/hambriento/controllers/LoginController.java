675d317bab6c2fa995ddf39c179b0ac4a9207c03
Osvaldo Gusmão <osvaldogusmao@Joses-MacBook-Pro.local>
2013-11-14 00:55:59 -0200

    DAO, Conexão e Persistencia

---
+++ src/main/java/br/edu/unifeob/hambriento/controllers/LoginController.java
@@ -0,0 +1,7 @@
+package br.edu.unifeob.hambriento.controllers;
+
+import org.springframework.stereotype.Repository;
+
+public class LoginController {
+    
+}
---
+++ src/main/java/br/edu/unifeob/hambriento/dao/base/DAO.java
@@ -0,0 +1,47 @@
+package br.edu.unifeob.hambriento.dao.base;
+
+import java.util.List;
+import java.util.Set;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+public abstract class DAO<T> {
+    
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public T load(Class<T> clazz, Integer id){
+        return entityManager.find(clazz, id);
+    }
+    
+    public T save(T entity){
+        T saved = null;
+        entityManager.getTransaction().begin();
+        saved = entityManager.merge(entity);
+        entityManager.getTransaction().commit();
+        return saved;
+    }
+    
+    public void remove(T entity){
+        entityManager.getTransaction().begin();
+        entityManager.remove(entity);
+        entityManager.getTransaction().commit();
+    }
+    
+    public void list(Class<T> clazz){
+        CriteriaQuery<T> criteria = entityManager.getCriteriaBuilder().createQuery(clazz);
+        Root<T> root = criteria.from(clazz);
+        Set<Root<?>> list = criteria.select(root).getRoots();
+        
+        System.out.println(list);
+        
+    }
+    
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+            
+            
+}
