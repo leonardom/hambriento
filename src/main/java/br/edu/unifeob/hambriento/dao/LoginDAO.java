eba55d6bf84879b488db0fcee6ac7026a5dca8e0
Osvaldo Gusmão <osvaldogusmao@Joses-MacBook-Pro.local>
2013-11-14 10:04:23 -0200

    DAO Generico

--- src/main/java/br/edu/unifeob/hambriento/dao/LoginDAO.java
+++ src/main/java/br/edu/unifeob/hambriento/dao/LoginDAO.java
@@ -2,14 +2,8 @@ package br.edu.unifeob.hambriento.dao;
 
 import br.edu.unifeob.hambriento.dao.base.DAO;
 import br.edu.unifeob.hambriento.entity.Login;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class LoginDAO extends DAO<Login>{
-    
-    @PersistenceContext
-    private EntityManager entityManager;
-    
+public class LoginDAO extends DAO<Login> {
 }
